16d04ca35aa279bacc0fe417479c77e609d6d2a3
dino nguyen and leo mangutov <leo@>
2019-01-16 14:19:27 -0800

    add hide method

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,11 @@
 $(document).ready(function() {
   $("form#appointment").submit(function(event){
     event.preventDefault();
-    var name = ($("#name").val());
-    var description = ($("#description").val());
-    var date = ($("#date").val());
-    var time = ($("#time").val());
+    var name = $("#name").val();
+    var description = $("#description").val();
+    var date = $("#date").val();
+    var time = $("#time").val();
+    $("#appointment").hide();
     var result = ("Your booking is successful!");
     $("#output").text(result);
   });
